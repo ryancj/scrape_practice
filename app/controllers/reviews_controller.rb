04a4a0dd4fae62832dc22984cc3ff96044094dbf
Ryan Kayu Ching <ryankching29@gmail.com>
2018-01-26 15:47:08 -0800

    Basic Review Crud

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @product          = Product.find params[:product_id]
+    review_params = params.require(:review).permit(:review_header, :reviewer, :rating, :review_body)
+    @review       = Review.new(review_params)
+    @review.product  = @product
+    if @review.save
+      redirect_to product_path(@product), notice: "Review created"
+    else
+      render "/products/show"
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :products
+  resources :products do
+    resources :reviews
+  end
 
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
